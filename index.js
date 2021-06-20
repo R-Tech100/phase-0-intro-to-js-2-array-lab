42d62dc78a4b982c4448f06dc533db8a97192337
QIURONG LIN <lint1006@gmail.com>
2021-06-20 02:16:37 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    name = 'Ralph';
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    name = "Bob";
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift(name);
+}
+
+function appendCat(name) {
+    return [...cats, name]
+}
+
+function prependCat(name) {
+    return [name, ...cats]
+}
+
+function removeLastCat() {
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
